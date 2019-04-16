d2a48b7e0cee83a2fadc190c644f2443285f3faa
matsuyoshi <sfbgwm30@gmail.com>
2019-04-17 00:47:08 +0900

    Improve initial screen.

--- panel/listPanel.go
+++ panel/listPanel.go
@@ -3,6 +3,7 @@ package panel
 import (
 	"fmt"
 	"net"
+	"time"
 
 	"gocap/packet"
 
@@ -15,6 +16,7 @@ type List struct {
 	Position
 	name    string
 	Packets []*Packet
+	stop    chan int
 }
 
 type Packet struct {
@@ -45,10 +47,25 @@ func (l *List) SetView(g *gocui.Gui) error {
 		v.Wrap = true
 		v.Frame = false
 
-		l.GetPacketList(v)
+		go l.Monitoring(l.stop, l.Gui.Gui, v)
 	}
 
 	return nil
+}
+
+func (l *List) Monitoring(stop chan int, g *gocui.Gui, v *gocui.View) {
+	ticker := time.NewTicker(1 * time.Second)
+
+LOOP:
+	for {
+		select {
+		case <-ticker.C:
+			l.GetPacketList(v)
+		case <-stop:
+			ticker.Stop()
+			break LOOP
+		}
+	}
 }
 
 func NewList(gui *Gui, name string, x, y, w, h int) *List {
@@ -63,14 +80,24 @@ func (l *List) Name() string {
 	return l.name
 }
 
-func (l *List) GetPacketList(v *gocui.View) {
+func (l *List) GetPacketList(v *gocui.View) error {
 	pds := packet.GetPacket()
 
 	for _, pd := range pds {
 		tp := pd.EData.EType
 		src := pd.EData.SrcMAC
 		dst := pd.EData.DstMAC
-		fmt.Fprintf(v, "%s: %s -> %s", tp, src, dst)
-		fmt.Fprintf(v, "\n")
+
+		l.Update(func(g *gocui.Gui) error {
+			v, err := l.View(l.name)
+			if err != nil {
+				return err
+			}
+			fmt.Fprintf(v, "%s: %s -> %s", tp, src, dst)
+			fmt.Fprintf(v, "\n")
+			return nil
+		})
 	}
+
+	return nil
 }
